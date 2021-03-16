27cf1d7bdc4c6814fafa4224018bfd7db26c0bd2
Andrea <andrea3aprile99@gmail.com>
2021-03-16 16:12:06 +0100

    s

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 
-var alertuno= alert(1,2,3,4,5,);
 
-
+var alertuno=[1,2,3,4,5];
+alert(alertuno.join(","))
 
 
 
 setTimeout(function () {
   var numero;
-  for (var i = 0; i < 5; i++){
+  var sbagliato = true;
+  var i = 0;
+  while ( i < 5 && sbagliato == true ){
     numero = parseInt(prompt('Inserisci un numero'));
-    if (numero !== alertuno[i]) {
-      consol.log("hai scordato i numeri")
+    if (!alertuno.includes(numero)) {
+      console.log("hai scordato i numeri");
+      sbagliato = false;
     }
+    i++;
   }
 }, 3000);
